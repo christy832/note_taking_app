f41eba493f551857a34d9afe3dad9a197cf958d0
christymoussallem24 <christy.moussallem@gotocme.com>
2026-01-03 16:57:28 +0200

    added recently deleted

--- app/src/main/java/com/example/christy_moussallem_mahmoud_abouchacra_tpnotee/Network/NotesApi.kt
+++ app/src/main/java/com/example/christy_moussallem_mahmoud_abouchacra_tpnotee/Network/NotesApi.kt
@@ -4,6 +4,7 @@ import com.example.christy_moussallem_mahmoud_abouchacra_tpnotee.Model.Note
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -12,6 +13,9 @@ interface NotesApi {
 
     @GET("notes")
     suspend fun getNotes(): List<Note>
+
+    @GET("notes/deleted")
+    suspend fun getDeletedNotes(): List<Note>
 
     @GET("notes/{id}")
     suspend fun getNote(@Path("id") id: Int): Note
@@ -25,6 +29,15 @@ interface NotesApi {
         @Body note: Note
     ): Note
 
+    // soft delete (move to trash)
     @DELETE("notes/{id}")
     suspend fun deleteNote(@Path("id") id: Int)
+
+    // restore from trash
+    @PATCH("notes/{id}/restore")
+    suspend fun restoreNote(@Path("id") id: Int): Note
+
+    // delete forever
+    @DELETE("notes/{id}/force")
+    suspend fun deleteNoteForever(@Path("id") id: Int)
 }
